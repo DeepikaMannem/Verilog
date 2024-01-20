dc9839fec01d962399e0cbb3ddc0e0482d3f3441
DeepikaMannem <153409915+DeepikaMannem@users.noreply.github.com>
2024-01-20 08:10:19 +0530

    design.v

--- Down Counter/design.v
+++ Down Counter/design.v
@@ -1,7 +1,7 @@
 module pedge(input bit clk, rst, output reg [3:0]count);
  reg [3:0] count_down = 0;
   always@(posedge clk) begin
-    if (rst | count_down == 0) begin
+    if (rst) begin
       count_down <= 4'b1111;
     end else begin
        count_down <= count_down - 1;
