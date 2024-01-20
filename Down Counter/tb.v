4bb923498df9fe2a126695f1921d0ca847807de6
DeepikaMannem <153409915+DeepikaMannem@users.noreply.github.com>
2024-01-20 07:57:14 +0530

    tb.v

---
+++ Down Counter/tb.v
@@ -0,0 +1,15 @@
+module tb;
+  wire [3:0] count;
+  reg clk, rst;
+  pedge dut (.clk(clk), .rst(rst),
+             .count(count));
+  initial begin
+    clk = 0; rst = 0;
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    #50 rst = 1;
+    #5 rst = 0;
+    #100 $finish;
+  end
+  always #2 clk = ~clk;
+endmodule
